15324a3c6b229c54e0b406a526836d4f71cca447
VNovytskyi <vladyslavnovytskyi2000@gmail.com>
2020-03-18 02:36:22 +0200

    Update NRF_ShiftRegister.c

--- NRF_ShiftRegister.c
+++ NRF_ShiftRegister.c
@@ -1,20 +1,21 @@
 extern SPI_HandleTypeDef hspi2;
 
-void SR_Set(void)
+inline void SR_Set(void)
 {
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET)
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
 }
 
-void SR_Reset(void)
+inline void SR_Reset(void)
 {
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
 }
 
 void SR_SetValue(uint16_t value)
 {
-	 HAL_Delay(1000);
-	 txBuff[0] = 0xff;
-	 txBuff[1] = 0xff;
-	 HAL_SPI_Transmit(&hspi2, txBuff, 2, 5000);
-	 cs_strob();
+	uint8_t txBuff[2] = {0};
+	txBuff[0] = 0xff;
+	txBuff[1] = 0xff;
+	HAL_SPI_Transmit(&hspi2, txBuff, 2, 5000);
+	SR_Reset();
+	SR_Set();
 }
